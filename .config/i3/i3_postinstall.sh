2a7ac978bfc6c7f5acbe18be278622fadefbeac6
Mehmet Can Boysan <mboysan.git@gmail.com>
2016-10-05 22:16:59 +0300

    i3

---
+++ .config/i3/i3_postinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+
+#install i3blocks
+sudo apt-get install i3blocks
+
+#install thunar file manager
+sudo apt-get install thunar
+
+#install icon-pack
+sudo apt-get install gnome-icon-theme-full
+
+#install brightness controllers
+sudo apt-get install xbacklight
+
+#install applets
+echo "install applets: mate-volume-control-applet"
+echo "install applets: mate-power-manager"
