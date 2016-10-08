4ac29517df9ad2a18f36b8eef31f2b8968fbdee1
Mehmet Can Boysan <mboysan.git@gmail.com>
2016-10-08 15:44:11 +0300

    modify default_apps.sh

--- scripts/default_apps.sh
+++ scripts/default_apps.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#### FILE MANAGER CHANGES ####
+#### CHANGE DEFAULT FILE MANAGER ####
 
 #See default file manager
 xdg-mime query default inode/directory
-#Set Thunar as default file manager
+
+#Set Thunar/pcmanfm as default file manager
 xdg-mime default Thunar.desktop inode/directory
+#xdg-mime default pcmanfm.desktop inode/directory
+
 #Test changes
 xdg-open $HOME
