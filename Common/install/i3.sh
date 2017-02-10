86fb3942940087e5d66f53c07569d8f8a49e3f92
Mehmet Can Boysan <mehmet.can.boysan@ut.ee>
2017-02-10 10:24:21 +0200

    add install and notes

---
+++ Common/install/i3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list﻿
+sudo apt-get update
+sudo apt-get --allow-unauthenticated install sur5r-keyring
+sudo apt-get update
+sudo apt-get install i3
